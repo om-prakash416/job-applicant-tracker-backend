d4979378415bbc83bfb8046da3572b9aac8598ec
Om Prakash <omprakash31181@gmail.com>
2025-07-18 16:05:02 +0530

    remove deprecated

--- src/helpers/dbConnection.js
+++ src/helpers/dbConnection.js
@@ -4,13 +4,15 @@ import { Message } from '../utils/constant/message.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DBURL);
-    logger.info(Message.MONGODB_CONNECTED);
-  } catch (error) {
-    logger.error(`${Message.MONGODB_CONNECTION_ERROR}`);
-    process.exit(1);
-};
 
-export default connectDB;
+  const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.DBURL); 
+      logger.info(Message.MONGODB_CONNECTED);
+    } catch (error) {
+      logger.error(`${Message.MONGODB_CONNECTION_ERROR} - ${error.message}`);
+      process.exit(1);
+    }
+  };
+
+  export default connectDB;
